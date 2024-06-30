3c4489a31ffbbebaf173a3ece93385bfe1aa43f6
jgarnet <jgarnett18@gmail.com>
2024-06-30 10:35:52 -0400

    updated README, added useMemo() to useSolveMaze()

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A React project that allows the user to build, edit, and solve mazes using [BFS](https://en.wikipedia.org/wiki/Breadth-first_search).
 
+Maze data is stored as a matrix in JSON files where `0` represents a path, `1` represents a wall, `2` represents the start point, and `3` represents the end point.
+
 # Build Maze
 
 ![build](./gifs/build-maze.gif)
--- src/hooks/useSolveMaze.ts
+++ src/hooks/useSolveMaze.ts
@@ -1,5 +1,6 @@
 import {PathPoint, Point} from "@/types";
 import {useMazeContext} from "@/context";
+import {useMemo} from "react";
 
 const DIRECTIONS: Point[] = [
     // down
@@ -13,45 +14,47 @@ const DIRECTIONS: Point[] = [
 ];
 export const useSolveMaze: () => PathPoint | null = () => {
     const { maze, start, end } = useMazeContext();
-    if (!maze || maze.length === 0 || !start || !end) {
-        return null;
-    }
-    // initialize queue for BFS algorithm
-    const queue: PathPoint[] = [];
-    // keep track of visited Points during BFS
-    const visited: boolean[][] = [];
-    // initialize visited[][] matrix, assigning false to all Points
-    for (let i = 0; i < maze.length; i++) {
-        visited[i] = [];
-        for (let j = 0; j < maze[i].length; j++) {
-            visited[i][j] = false;
+    return useMemo(() => {
+        if (!maze || maze.length === 0 || !start || !end) {
+            return null;
         }
-    }
-    const isValidPoint = (point: Point) => {
-        return (
-            // ensure Point bounds are valid
-            point.y >= 0 && point.y < maze.length &&
-            point.x >= 0 && point.x < maze[point.y].length &&
-            // ensure Point is not already visited
-            !visited[point.y][point.x] &&
-            // ensure Point is not a wall
-            maze[point.y][point.x] !== 1
-        );
-    };
-    // begin BFS algorithm
-    queue.unshift(start as Point);
-    while (queue.length > 0) {
-        const currentPoint = queue.shift() as PathPoint;
-        if (currentPoint.x === end.x && currentPoint.y === end.y) {
-            return currentPoint;
-        }
-        for (const direction of DIRECTIONS) {
-            const nextPoint = { x: currentPoint.x + direction.x, y: currentPoint.y + direction.y };
-            if (isValidPoint(nextPoint)) {
-                visited[nextPoint.y][nextPoint.x] = true;
-                queue.unshift({ ...nextPoint, prev: currentPoint });
+        // initialize queue for BFS algorithm
+        const queue: PathPoint[] = [];
+        // keep track of visited Points during BFS
+        const visited: boolean[][] = [];
+        // initialize visited[][] matrix, assigning false to all Points
+        for (let i = 0; i < maze.length; i++) {
+            visited[i] = [];
+            for (let j = 0; j < maze[i].length; j++) {
+                visited[i][j] = false;
             }
         }
-    }
-    return null;
+        const isValidPoint = (point: Point) => {
+            return (
+                // ensure Point bounds are valid
+                point.y >= 0 && point.y < maze.length &&
+                point.x >= 0 && point.x < maze[point.y].length &&
+                // ensure Point is not already visited
+                !visited[point.y][point.x] &&
+                // ensure Point is not a wall
+                maze[point.y][point.x] !== 1
+            );
+        };
+        // begin BFS algorithm
+        queue.unshift(start as Point);
+        while (queue.length > 0) {
+            const currentPoint = queue.shift() as PathPoint;
+            if (currentPoint.x === end.x && currentPoint.y === end.y) {
+                return currentPoint;
+            }
+            for (const direction of DIRECTIONS) {
+                const nextPoint = { x: currentPoint.x + direction.x, y: currentPoint.y + direction.y };
+                if (isValidPoint(nextPoint)) {
+                    visited[nextPoint.y][nextPoint.x] = true;
+                    queue.unshift({ ...nextPoint, prev: currentPoint });
+                }
+            }
+        }
+        return null;
+    }, [maze]);
 };
