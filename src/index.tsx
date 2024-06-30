da272458f0bdb173722fe974eb8005a927a64be7
jgarnet <jgarnett18@gmail.com>
2024-06-30 09:25:22 -0400

    updated component import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import {createRoot} from 'react-dom/client';
 import React from 'react';
 import './index.scss';
-import {App} from '@/components/App/App';
+import {App} from '@/components';
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
