b1527aedd0b931f23b563988fb79baf9d1a7ec91
jgarnet <jgarnett18@gmail.com>
2024-06-30 09:25:06 -0400

    fixed media type

--- src/components/MazeExporter/MazeExporter.tsx
+++ src/components/MazeExporter/MazeExporter.tsx
@@ -4,7 +4,7 @@ export const MazeExporter = () => {
     const { maze } = useMazeContext();
     const exportMaze = () => {
         const a = document.createElement('a');
-        a.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(maze))
+        a.href = 'data:application/json;charset=utf-8,' + JSON.stringify(maze)
         a.target = '_blank';
         a.download = 'maze.json';
         a.click();
