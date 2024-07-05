23d9641d9f6d99efa912643e586891e20c81f0a7
jgarnet <jgarnett18@gmail.com>
2024-07-05 14:22:41 -0400

    fixed start and end editor state when maze is generated

--- src/components/GenerateMaze/GenerateMaze.tsx
+++ src/components/GenerateMaze/GenerateMaze.tsx
@@ -9,11 +9,13 @@ type GenerateMazeProps = {
 
 export const GenerateMaze: FC<GenerateMazeProps> = props => {
     const { onGenerate } = props;
-    const { maze, setMaze } = useMazeContext();
+    const { maze, setMaze, setStart, setEnd } = useMazeContext();
     const generateMaze = useMazeGenerator();
     const onClick = () => {
         const _maze = generateMaze(maze.length, maze[0].length);
         setMaze(_maze);
+        setStart({ x: 0, y: 0 });
+        setEnd({ x: maze[0].length - 1, y: maze.length - 1 });
         if (onGenerate) {
             onGenerate(_maze);
         }
